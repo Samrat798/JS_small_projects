a6aac590eb52d446411ec28bb13c633d8137607d
Samrat798 <majumdersamrat26@gmail.com>
2024-08-20 20:45:44 +0530

    aster

--- todoList/app.js
+++ todoList/app.js
@@ -1,19 +1,45 @@
-const taskInput = document.querySelector('.input-task');
-const addBtn = document.querySelector('.add');
-const taskContainer = document.querySelector('.task-list');
+const form = document.querySelector('.form-control');
+const inputTask = document.getElementById('todo-input');
+const submitBtn = document.querySelector('.todo-submit');
+const todoContainer = document.querySelector('.todo-container');
+const todoList = document.querySelector('.todo-list');
 
-const taskList = [];
+const todos = [];
 
-function addTask(e) {
+submitBtn.addEventListener('click', addItem);
+
+function addItem(e) {
   e.preventDefault();
-  const newTask = {
-    task: taskInput.value,
-    id: Date.now(),
-    complete: false,
+  if (inputTask.value === '') {
+    alert('Enter a task...');
+    return;
+  }
+  let newTask = {
+    task: inputTask.value,
+    id: new Date().getTime().toString(),
+    edit: false,
   };
-  taskList.push(newTask);
-  taskInput.value = '';
-  console.log(taskList);
+  todos.push(newTask);
+  inputTask.value = '';
+  console.log(todos);
 }
 
-addBtn.addEventListener('click', addTask);
+// function createListItem(item) {
+//   const element = document.createElement('article');
+//   element.classList.add('todo-item');
+//   const attr = document.createAttribute('data-id');
+//   attr.value = id;
+//   element.setAttribute(attr);
+//   element.innerHTML = `
+//     <p class="title">${value}</p>
+//     <div class="btn-container">
+//       <button type="button" class="edit-btn">
+//         <i class="fas fa-edit"></i>
+//       </button>
+//       <button type="button" class="delete-btn">
+//         <i class="fas fa-trash"></i>
+//       </button>
+//     </div>
+//   `;
+//   todoList.appendChild(element);
+// }
