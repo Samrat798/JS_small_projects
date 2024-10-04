b7c5ce6dfb533f459d5feecaa8f2a8723a757230
Samrat798 <majumdersamrat26@gmail.com>
2024-10-04 10:11:48 +0530

    aster

--- todo_List/script.js
+++ todo_List/script.js
@@ -1,24 +1,14 @@
-const tasks = [];
+let tasks = [];
 
 const input = document.querySelector('.input');
 const add = document.querySelector('.add');
 const listDom = document.querySelector('.list');
 
+window.addEventListener('load', getFromLocalStorage());
+
 add.addEventListener('click', function (e) {
   e.preventDefault();
   addTask();
-  listDom.innerHTML = '';
-  tasks.map((task) => {
-    const li = document.createElement('li');
-    li.classList.add('list-item');
-    li.innerHTML = `${task.task}
-    <div class="btn-container">
-            <button class="btn delete">Delete</button> 
-            <button class="btn edit">Edit</button>
-          </div>
-    `;
-    listDom.appendChild(li);
-  });
 });
 
 function addTask() {
@@ -32,6 +22,35 @@ function addTask() {
     done: false,
   };
   tasks.push(newTask);
+  saveToLocalStorage(tasks);
   input.value = '';
   console.log(tasks);
+}
+
+function displayTask(tasks) {
+  listDom.innerHTML = '';
+  tasks.map((task) => {
+    const li = document.createElement('li');
+    li.classList.add('list-item');
+    li.innerHTML = `${task.task}
+    <div class="btn-container">
+            <button class="btn delete">Delete</button> 
+            <button class="btn edit">Edit</button>
+          </div>
+    `;
+    listDom.appendChild(li);
+  });
+}
+
+function saveToLocalStorage(todos) {
+  localStorage.setItem('todos', JSON.stringify(todos));
+  displayTask(todos);
+}
+
+function getFromLocalStorage() {
+  const storedTodos = localStorage.getItem('todos');
+  if (storedTodos) {
+    tasks = JSON.parse(storedTodos);
+    displayTask(tasks);
+  }
 }
